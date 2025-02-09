65d90fffb4e9b73460307dd0efc124932d1a53ee
Rex loriila <lorillarex_bsit@plmun.edu.ph>
2025-02-10 00:55:46 +0800

    Submitting Python Week 02 - Session 01 - Exercise 01

--- exercise_01.py
+++ exercise_01.py
@@ -1,10 +1,11 @@
-count1 = 10                   # 'count' is assigned 10 (integer literal)
-Count2 = 15                   # 'Count' (different from 'count') is assigned 15
+count = 10                   # 'count' is assigned 10 (integer literal)
+Count = 15                   # 'Count' (different from 'count') is assigned 15
 total_count = 20             # Another integer literal assignment
 decimal_value = 3.14         # 'decimal_value' is assigned 3.14 (float literal)
 message = "Hello, Python!"   # String literal
 is_active = True             # Boolean literal
 result = None                # None literal represents absence of value
+
 
 print("Integer (count):", count)
 print("Integer (Count):", Count)
@@ -13,5 +14,4 @@ print("Decimal:", decimal_value)
 print("Text:", message)
 print("Boolean:", is_active)
 print("None Value:", result)
-print(f"total_count: {count1 + count2:.2f}")
-
+print(f"Sum: {count + count:.2f}")
